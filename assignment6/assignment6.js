7488cc28ab04f12fd0eaaa7c5dfa396f6e4be005
Marc Bizal <marc.bizal@gmail.com>
2015-12-10 16:19:40 -0500

    Started JS for Assignment 6

---
+++ assignment6/assignment6.js
@@ -0,0 +1,19 @@
+var hint = {
+    "state" : "Use a two letter abbreviation",
+    "zip" : "Use a 5 digit Zip Code",
+    "phone" : "Numbers Only – No Spaces or Dashes",
+    "email" : "Example – john@doe.com",
+    "card_number" : "Numbers Only – No Spaces or Dashes"
+};
+
+var error = {
+    {
+        "appliesTo" : ["first_name", "last_name", "city"],
+        "message" : "Required: Must contain only letters and spaces between 2 and 20 characters!"
+    },
+
+};
+
+$(document).ready(function() {
+
+});
